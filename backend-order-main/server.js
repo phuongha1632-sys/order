37ffd02e8819e71d86cf2c187c48dfee11ae378e
nguyendinhhung-ai <nguyendinhhung01012025@gmail.com>
2025-12-11 00:18:39 +0700

    update backend fix upload ảnh + phân quyền admin/client
    Add static file serving for uploads directory

--- backend-order-main/server.js
+++ backend-order-main/server.js
@@ -1,23 +1,26 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Import DB để kết nối ngay khi server khởi động
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 require("./config/database");
-// Import routes
+
 const menuRoutes = require("./routes/menu.routes");
 app.use("/api/menu", menuRoutes);
 
-// Route test
+// Test route
 app.get("/", (req, res) => {
     res.send("API is running...");
 });
 
-// Chạy server
+// Run server
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server đang chạy tại http://localhost:${PORT}`);
