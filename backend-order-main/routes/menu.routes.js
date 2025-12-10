ced30ad74c253492503fcc20ed839c21a82a18b4
nguyendinhhung-ai <nguyendinhhung01012025@gmail.com>
2025-12-11 00:17:59 +0700

    update backend fix upload ảnh + phân quyền admin/client

--- backend-order-main/routes/menu.routes.js
+++ backend-order-main/routes/menu.routes.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
 const menuController = require("../controllers/menu.controller");
 
-// API menu
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, "uploads/"),
+    filename: (req, file, cb) =>
+        cb(null, Date.now() + path.extname(file.originalname))
+});
+
+const upload = multer({ storage });
+
 router.get("/client", menuController.getClientMenu);
 router.get("/admin", menuController.getAdminMenu);
-router.post("/", menuController.addMenuItem);
-router.put("/:id", menuController.updateMenuItem);
+router.post("/", upload.single("image"), menuController.addMenuItem);
+router.put("/:id", upload.single("image"), menuController.updateMenuItem);
 router.delete("/:id", menuController.deleteMenuItem);
+
 module.exports = router;
